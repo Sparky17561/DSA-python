f310cf922efc5aae388e1abd0ec7717c33298d80
Saiprasad Jamdar <saiprasadjamdar@gmail.com>
2025-05-06 20:54:32 +0530

    dsa all done

--- anagramString.py
+++ anagramString.py
@@ -1,23 +1,23 @@
-def is_anagram(st1, st2):
-    if len(st1) != len(st2):
-        return False
-
-    count = {}
-
-    for char in st1:
-        if char in count:
-            count[char] += 1
-        else:
-            count[char] = 1
-
-    for char in st2:
-        if char not in count or count[char] == 0:
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
             return False
-        count[char] -= 1
+        
+        count ={}
 
-    return True
-
-st1 = input().lower()
-st2 = input().lower()
-
-print(is_anagram(st1, st2))
+        for char in s:
+            if char in count:
+                count[char] +=1
+            else:
+                count[char] = 1
+        
+        for char in t:
+            if char not in count or count[char] == 0:
+                return False
+            count[char] -=1
+        return True
